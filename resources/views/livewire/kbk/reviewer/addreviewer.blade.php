3916129e3632adcb6a215d363feeab5b881d3023
rhesaaji <rhesaaji@upi.edu>
2023-06-19 10:59:19 +0700

    commit4-rhesa

--- resources/views/livewire/kbk/reviewer/addreviewer.blade.php
+++ resources/views/livewire/kbk/reviewer/addreviewer.blade.php
@@ -4,37 +4,73 @@
             <b>Research Data</b>
         </div>
     </div>
-    <div class="card-body">
-        <div class="row">
-            <div>1</div> <div>E0451.190025 I Elektronika Indsutri</div>
-            <div class="col-md-6" style="border-right:solid;border-right-width:1px">
-                <div><b>Reviewer(s) of Student's Proposal</b>
-                    <div>(jika belum di add ada tulisan the research reviewer should be assigned, jika sudah di add tampilkan nama dosen dan code dosen)</div>
-                </div>
-                <br>
-                <div><button style="background-color: aqua"><i class="fas fa-user-plus"></i>Assign</button></div>
+    <div class="card">
+        <div class="card-header">
+              <div class="card-tools">
+            <!-- Collapse Button -->
+            <button type="button" class="btn btn-tool" data-card-widget="collapse">Click to fold/unfold&nbsp;&nbsp;<i class="fas fa-arrow-down text-red" ></i></button>
+          </div>
+          <!-- /.card-tools -->
+          <div class="row">
+            <div style="width: 10%;color:red;vertical-align:middle;font-size:30pt" class="text-center">1</div>
+            <div style="width:1%"></div>
+            <div style="width: 30%;border-right:solid;border-right-width:1px">
+                <div class="row"><div style="font-style:italic">&nbsp;&nbsp;E4051.1900255</div>&nbsp;| Elektronika Industri</div>
+                <div>PTE</div>
+                <div><b>Wili Wiliana</b></div>
             </div>
-            <div class="col-md-6">
-                <div><b>Supervisor of Student's Proposal</b>
-                    <div>(jika belum di add ada tulisan the research reviewer should be assigned, jika sudah di add tampilkan nama dosen dan code dosen)</div>
+            <div style="width:1%"></div>
+            <div style="width: 30%">
+                <div>SK-1900255 | <b>Proposal</b></div>
+                <div style="font-style:italic">Research is being reviewed</div>
+            </div>
+          </div>
+          <b>Judul Penelitian</b>
+          <div style="font-style:italic">File of proposal could be access &nbsp;<a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/folders/1BsWl8BS-Eo41KRQrfNXpK94WxJclhc1q"><i class="fa fa-file-pdf"></i> <u>here</u></a></div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body"> 
+    
+            <div class="row">
+                
+                <div class="col-md-6" style="border-right:solid;border-right-width:1px">
+                    <div><b>Reviewer(s) of Student's Proposal</b>
+                        <div>(jika belum di add ada tulisan the research reviewer should be assigned, jika sudah di add tampilkan nama dosen dan code dosen)</div>
+                    </div>
+                    <br>
+                    <div><button style="background-color: aqua"><i class="fas fa-user-plus"></i>Assign</button></div>
                 </div>
-                <br>
-                <div><button style="background-color: aqua"><i class="fas fa-user-plus"></i>Assign</button></div>
+                <div class="col-md-6">
+                    <div><b>Supervisor of Student's Proposal</b>
+                        <div>(jika belum di add ada tulisan the research reviewer should be assigned, jika sudah di add tampilkan nama dosen dan code dosen)</div>
+                    </div>
+                    <br>
+                    <div><button style="background-color: aqua"><i class="fas fa-user-plus"></i>Assign</button></div>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div><i class="fas fa-quote-right">  </i><b>Review Decision</b></div>
+            <div class="row">
+                <div><button style="background-color: rgba(205, 11, 11, 0.874);color:#ffff"><b>Reject</b></button></div>
+                <div style="width:1%"></div>
+                <div><button style="background-color: rgb(210, 159, 5);color:#080000"><b>Revise</b></button></div>
+                <div style="width:1%"></div>
+                <div><button style="background-color: rgba(13, 93, 220, 0.874);color:#ffff"><b>Presentation</b></button></div>
+                <div style="width:1%">
+                </div><div><button style="background-color: rgba(6, 156, 48, 0.874);color:#ffff"><b>Approve</b></button></div>
+                <div style="width:1%">
+                </div><div><button style="background-color: rgba(7, 155, 228, 0.874);color:#ffff"><b>Add Comment</b></button></div>
+            </div>
+            <br>
+            <br>
+            <div><i class="fas fa-quote-right"></i><b>Review Discussion</b>  </div>
+            <div class="col-sm-12 text-right">
+        
+                <div><button style="background-color: rgba(10, 197, 197, 0.833);color:aliceblue"><b>Add Discussion</b></button></div>
             </div>
         </div>
-        <br>
-        <br>
-        <div><b>Decision</b></div>
-        <div class="row">
-            <div><button style="background-color: rgba(205, 11, 11, 0.874);color:#ffff"><b>Reject</b></button></div>
-            <div style="width:1%"></div>
-            <div><button style="background-color: rgb(210, 159, 5);color:#080000"><b>Revise</b></button></div>
-            <div style="width:1%"></div>
-            <div><button style="background-color: rgba(13, 93, 220, 0.874);color:#ffff"><b>Presentation</b></button></div>
-            <div style="width:1%">
-            </div><div><button style="background-color: rgba(6, 156, 48, 0.874);color:#ffff"><b>Approve</b></button></div>
-            <div style="width:1%">
-            </div><div><button style="background-color: rgba(7, 155, 228, 0.874);color:#ffff"><b>Add Comment</b></button></div>
-        </div>
-    </div>
+        <!-- /.card-body -->
+      </div>
+    
 </div>
